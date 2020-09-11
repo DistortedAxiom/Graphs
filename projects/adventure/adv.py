3b8b6034b730ad16aad0a12f7162d938d265c65d
Disto <48456332+DistortedAxiom@users.noreply.github.com>
2020-09-11 12:34:00 -0400

    Sprint finished 994 moves

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -29,6 +29,52 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+# Create a dictionary containing the opposite direction for us to backtrack
+reversed_directions = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'}
+
+# Add a reverse path to keep track for backtracking
+reversal_path = []
+
+# room_dict = {}
+# for x in range(0, len(room_graph)):
+#    room_dict[x] = {'n': '?', 's': '?', 'w': '?', 'e': '?'}
+
+# Create a set containing the ROOMS that we've visited
+visited = set()
+
+# While the amount of room we've visited is less than the total amount of rooms
+while len(visited) < len(room_graph):
+
+    # Initializing, we'll use this variable to dictate where we WANT to travel next
+    next_move = None
+
+    # Go through the exits in the current room
+    for exit in player.current_room.get_exits():
+        # If we haven't visited one of the room connected yet
+        if player.current_room.get_room_in_direction(exit) not in visited:
+            # Set that as the destination to travel
+            next_move = exit
+            break
+
+    # If we have a direction that we can go to (not dead end)
+    if next_move is not None:
+        # Add the movement to our traversal path
+        traversal_path.append(next_move)
+        # We also want to add the opposite direction of where we're going for backtracking later
+        reversal_path.append(reversed_directions[next_move])
+        # We finally move the player to the room
+        player.travel(next_move)
+        # Add the new room that we are in now to the set of visited room
+        visited.add(player.current_room)
+
+    # If there's no more exit that we have not visited (dead end)
+    else:
+        # We will set the destination for the next move to be the previous path we've taken
+        next_move = reversal_path.pop()
+        # Add the backtracking step to the traversal path
+        traversal_path.append(next_move)
+        # Finally, move the PLAYER to the previous room
+        player.travel(next_move)
 
 
 # TRAVERSAL TEST
@@ -51,12 +97,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
